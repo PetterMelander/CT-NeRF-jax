c38a7fb87e615fb40c95df5fe84bd6fd2110fd7b
Petter Melander <petter.melander@nordaxon.com>
2024-10-24 18:51:21 +0200

    small update to generate_xrays script

--- scripts/generate_xrays.py
+++ scripts/generate_xrays.py
@@ -3,15 +3,11 @@
 from ctnerf.utils import get_ct_dir, get_xray_dir
 from ctnerf.xray_creation import generate_xrays
 
-ct_dir = get_ct_dir()
-xray_dir = get_xray_dir()
-
 max_angle = 180
 angle_interval_size = 1
-ct_path = ct_dir / "nrrd" / "2 AC_CT_TBody.nrrd"
+ct_path = get_ct_dir() / "nrrd" / "2 AC_CT_TBody.nrrd"
 device = "cuda:0"
 
-# output_dir = datapath / "xrays" / ct_path.stem  # noqa: ERA001
-output_dir = xray_dir / "xrays" / "test"
+output_dir = get_xray_dir() / "xrays" / "test"
 
 generate_xrays(ct_path, output_dir, angle_interval_size, max_angle)
