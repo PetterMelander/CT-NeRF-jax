01d67011094651b9b39e26048ca174e68e32b652
Petter Melander <petter.melander@nordaxon.com>
2024-10-26 21:55:50 +0200

    Updated and documented configs

--- scripts/train.py
+++ scripts/train.py
@@ -7,7 +7,10 @@ from ctnerf.utils import get_config_dir
 
 torch.set_float32_matmul_precision("high")
 torch.backends.cudnn.benchmark = True
-torch.backends.cuda.allow_tf32 = True
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction = True
+torch.backends.cuda.matmul.allow_bf16_reduced_precision_reduction = True
+torch.backends.cudnn.allow_tf32 = True
 torch.backends.cuda.preferred_blas_library("cublaslt")
 
 if __name__ == "__main__":
