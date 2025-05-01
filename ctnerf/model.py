fix image tracking, fix experiment tracking, and make small adjustment to dtypes in positional_encoding (float16 is now allowed if it will not overflow)

ctnerf/model.py
@@ -179,8 +179,8 @@ def _positional_encoding(coords: jax.Array, L: int, policy: jmp.Policy) -> jax.A
 
     """
     dtype = (
-        jnp.float32 if policy.compute_dtype == jnp.float16 else policy.compute_dtype
-    )  # float16 will overflow if L >= 15
+        jnp.float32 if policy.compute_dtype == jnp.float16 and L > 15 else policy.compute_dtype  # noqa: PLR2004
+    )  # float16 will overflow if L >= 16
     freq_bands = jnp.power(2.0, jnp.arange(L, dtype=dtype)) * jnp.pi
     angles = jnp.expand_dims(coords, axis=-1) * jnp.expand_dims(freq_bands, axis=0)
     angles = policy.cast_to_compute(angles.reshape(-1))

ctnerf/training/training.py
@@ -208,8 +208,10 @@ def _eval(
         mae = np.mean(np.abs(generated_ct - source_ct_image))
         aim_run.track(mae, name="mae", context={"model": tag})
 
-        ct_slice = generated_ct[:, :, generated_ct.shape[2] // 2]
-        image = PILImage.fromarray((ct_slice.astype(np.int32) + 1024).astype(np.uint32))
+        ct_slice = generated_ct[::-1, :, generated_ct.shape[2] // 2]
+        image = PILImage.fromarray(
+            ((ct_slice.astype(np.float32) + 1024) / 4095 * 255).astype(np.uint8),
+        )
         aim_run.track(
             Image(image),
             name="cross section",